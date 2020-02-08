b8593f03ca922083e94181a609d458896d811346
Ilya Chesalin <evilyach@protonmail.com>
2020-02-08 17:52:22 +0300

    Added backend router

---
+++ frontend/src/router/backend.js
@@ -0,0 +1,11 @@
+export default {
+  host: "http://localhost:8080",
+
+  register() {
+    return this.host + "/api/v1/auth/register";
+  },
+
+  login() {
+    return this.host + "/api/v1/auth/login";
+  }
+};
