38b78a519a929789a485af37ff4e280d9c27d6aa
Ilya Chesalin <evilyach@protonmail.com>
2020-02-08 18:18:37 +0300

    Fixed router linter

--- frontend/src/router/backend.js
+++ frontend/src/router/backend.js
@@ -1,11 +1,11 @@
 export default {
-  host: "http://localhost:8080",
+  host: 'http://localhost:8080',
 
-  register() {
-    return this.host + "/api/v1/auth/register";
+  register () {
+    return this.host + '/api/v1/auth/register'
   },
 
-  login() {
-    return this.host + "/api/v1/auth/login";
+  login () {
+    return this.host + '/api/v1/auth/login'
   }
-};
+}
