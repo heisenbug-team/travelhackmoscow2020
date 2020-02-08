c5fa3ed4d1c4c56d982fa813c318ddce69334129
Ilya Chesalin <evilyach@protonmail.com>
2020-02-08 18:19:25 +0300

    [#2] Modified register page

--- frontend/src/pages/auth/index.vue
+++ frontend/src/pages/auth/index.vue
@@ -6,91 +6,126 @@
 
     <vertical-separator />
 
-    <q-form @submit="onSubmit" class="q-gutter-md">
-      <q-stepper v-model="step" ref="stepper" color="primary" animated contracted>
-        <q-step :name="1" icon="person">
-          <q-card-section align="center">
-            Добро пожаловать
+    <q-stepper v-model="step" ref="stepper" color="primary" animated contracted>
+      <q-step :name="1" icon="person" title="Приветствие">
+        <q-card-section align="center">
+          Добро пожаловать
 
-            <q-btn-toggle
-              v-model="mode"
-              toggle-color="primary"
-              :options="mode_options"
-            />
-          </q-card-section>
-        </q-step>
+          <q-btn-toggle
+            v-model="mode"
+            toggle-color="primary"
+            :options="mode_options"
+          />
+        </q-card-section>
+      </q-step>
 
-        <q-step :name="2" icon="account_circle">
-          <q-card-section align="center">
-            <q-input filled v-model="nickname" label="Псевдоним" hint="Введите псевдоним" required />
+      <q-step :name="2" icon="account_circle" title="Личные данные">
+        <q-card-section align="center">
+          <q-input
+            filled
+            v-model="credentials.nickname"
+            label="Псевдоним"
+            hint="Введите псевдоним"
+          />
 
-            <vertical-separator />
+          <vertical-separator />
 
-            <q-input filled v-model="name" label="Имя" hint="Введите имя" required />
+          <q-input
+            filled
+            v-model="credentials.name"
+            label="Имя"
+            hint="Введите имя"
+          />
 
-            <vertical-separator />
+          <vertical-separator />
 
-            <q-input
-              filled
-              v-model="email"
-              label="Адрес электронной почты"
-              hint="Введите адрес электронной почты"
-              required
-            />
-          </q-card-section>
-        </q-step>
+          <q-input
+            filled
+            v-model="credentials.email"
+            label="Адрес электронной почты"
+            hint="Введите адрес электронной почты"
+          />
 
-        <q-step :name="3" icon="language">
-          <q-card-section align="center">
-            <p>Выберете языки, на которых вы готовы общаться:</p>
-            <q-option-group
-              type="checkbox"
-              :options="languages_options"
-              v-model="languages_selected"
-            />
-          </q-card-section>
-        </q-step>
+          <vertical-separator />
 
-        <q-step :name="4" icon="insert_emoticon">
-          <q-card-section align="center">
-            <p>Выберете интересы:</p>
-            <q-option-group
-              label="Интересы"
-              type="checkbox"
-              :options="interests_options"
-              v-model="interests_selected"
-            />
-          </q-card-section>
-        </q-step>
+          <q-input
+            filled
+            v-model="credentials.password"
+            label="Пароль"
+            :type="credentials.isPwd ? 'password' : 'text'"
+            hint="Введите пароль"
+          >
+            <template v-slot:append>
+              <q-icon
+                :name="credentials.isPwd ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="credentials.isPwd = !credentials.isPwd"
+              />
+            </template>
+          </q-input>
+        </q-card-section>
+      </q-step>
 
-        <template v-slot:navigation>
-          <q-stepper-navigation align="center">
-            <q-btn
-              @click="$refs.stepper.next()"
-              color="primary"
-              :label="step === 4 ? 'Завершить регистрацию' : 'Продолжить'"
-            />
-            <q-btn
-              v-if="step > 1"
-              color="secondary"
-              @click="$refs.stepper.previous()"
-              label="Назад"
-              class="q-ml-sm"
-            />
-          </q-stepper-navigation>
-        </template>
-      </q-stepper>
-    </q-form>
+      <q-step :name="3" icon="language" title="Языки">
+        <q-card-section align="center">
+          <p>Выберете языки, на которых вы готовы общаться:</p>
+          <q-option-group
+            type="checkbox"
+            :options="languages_options"
+            v-model="languages_selected"
+          />
+        </q-card-section>
+      </q-step>
+
+      <q-step :name="4" icon="insert_emoticon" title="Интересы">
+        <q-card-section align="center">
+          <p>Выберете интересы:</p>
+          <q-option-group
+            label="Интересы"
+            type="checkbox"
+            :options="interests_options"
+            v-model="interests_selected"
+          />
+        </q-card-section>
+      </q-step>
+
+      <template v-slot:navigation>
+        <q-stepper-navigation align="center">
+          <q-btn
+            @click="step === 4 ? submit() : $refs.stepper.next()"
+            color="primary"
+            type="submit"
+            :label="step === 4 ? 'Завершить регистрацию' : 'Продолжить'"
+          />
+          <q-btn
+            v-if="step > 1"
+            color="secondary"
+            @click="$refs.stepper.previous()"
+            label="Назад"
+            class="q-ml-sm"
+          />
+        </q-stepper-navigation>
+      </template>
+    </q-stepper>
   </div>
 </template>
 
 <script>
 import verticalSeparator from '../../components/common/vertical-separator'
 import logo from '../../components/brand/logo'
+import backendRouter from '../../router/backend'
+
+import axios from 'axios'
 
 export default {
   data () {
     return {
+      credentials: {
+        nickname: '',
+        name: '',
+        password: '',
+        isPwd: true
+      },
       step: 1,
       mode: 'tourist',
       mode_options: [
@@ -120,6 +155,30 @@ export default {
         { label: 'Автомобили', value: 'chinese' },
         { label: 'Автомобили', value: 'chinese' }
       ]
+    }
+  },
+  methods: {
+    async submit () {
+      axios.post(backendRouter.register(), {
+        mode: this.mode,
+        nickname: this.credentials.nickname,
+        name: this.credentials.name,
+        password: this.credentials.password,
+        languages: this.languages_selected,
+        interests: this.interests_selected
+      })
+        .then(async () => {
+          await this.$router.push('/')
+          window.location.reload()
+        })
+        .catch((error) => {
+          console.error(error.message)
+
+          this.$q.notify({
+            message: 'Регистрация не удалась. :(',
+            color: 'negative'
+          })
+        })
     }
   },
   components: {
